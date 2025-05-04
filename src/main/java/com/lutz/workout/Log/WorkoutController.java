6d1baec0914d4d53af3a471cb5428bdb5475e46e
benjaminlutz <benlutz@pdx.edu>
2025-05-04 15:47:30 -0700

    get methods for controller

---
+++ src/main/java/com/lutz/workout/Log/WorkoutController.java
@@ -0,0 +1,32 @@
+package com.lutz.workout.Log;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/workout-logs")
+public class WorkoutController {
+    private final WorkoutLogRepository workoutLogRepository;
+
+    public WorkoutController(WorkoutLogRepository workoutLogRepository) {
+        this.workoutLogRepository = workoutLogRepository;
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("")
+    public List<WorkoutLog> getWorkoutLogs() {
+        return workoutLogRepository.getWorkoutLogs();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    public WorkoutLog getWorkoutLogById(@PathVariable Integer id) {
+        return workoutLogRepository.getWorkoutLogs().stream()
+                .filter(workoutLog -> workoutLog.id().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new WorkoutLogNotFoundException());
+    }
+
+}
---
+++ src/main/java/com/lutz/workout/Log/WorkoutLogNotFoundException.java
@@ -0,0 +1,11 @@
+package com.lutz.workout.Log;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class WorkoutLogNotFoundException extends RuntimeException {
+    public WorkoutLogNotFoundException() {
+        super("Workout log not found: ");
+    }
+}
